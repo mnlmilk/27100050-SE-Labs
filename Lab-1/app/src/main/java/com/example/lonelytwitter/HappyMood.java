9c7647b8b1c349933579ed016a4115e25bb6a9e9
mnlmilk <27100050@lums.edu.pk>
2026-01-27 11:30:27 +0500

    Lab-1 app submission

---
+++ Lab-1/app/src/main/java/com/example/lonelytwitter/HappyMood.java
@@ -0,0 +1,20 @@
+package com.example.lonelytwitter;
+
+import java.util.Date;
+
+public class HappyMood extends Mood {
+    public HappyMood() {
+        super();
+    }
+
+    public HappyMood(Date date) {
+        super(date);
+    }
+
+
+
+    @Override
+    public String getMoodString() {
+        return "What a lovely day!";
+    }
+}
---
+++ Lab-1/app/src/main/java/com/example/lonelytwitter/SadMood.java
@@ -0,0 +1,18 @@
+package com.example.lonelytwitter;
+
+import java.util.Date;
+public class SadMood extends Mood {
+
+    public SadMood() {
+        super();
+    }
+
+    public SadMood(Date date) {
+        super(date);
+    }
+
+    @Override
+    public String getMoodString() {
+        return "What a gloomy day :c";
+    }
+}
